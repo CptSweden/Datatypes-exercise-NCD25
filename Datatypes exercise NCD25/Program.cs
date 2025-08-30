fac76bace2dafb9ed5755730f09c2cf89700089f
joelb <joelb@DESKTOP-P8KJ30R>
2025-08-30 14:31:26 +0200

    Add project files.

---
+++ Datatypes exercise NCD25/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Datatypes_exercise_NCD25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = "Joel";
+
+            string lastName = "Boqvist";
+
+            string hobby = "Music";
+
+            int age = 31;
+
+            int year = 2025;
+
+            int favoriteNumber = 14;
+
+            double temperature = 18.1;
+
+            double pi = 3.14;
+
+            double height = 1.72;
+
+            bool isStudent = true;
+
+            bool likesPizza = true;
+
+            bool hasPet = true;
+
+            char grade = 'A';
+
+            char middleInitial = 'K';
+
+            char middleInitial2 = 'V';
+
+            char currencySymbol = '$';
+
+            Console.WriteLine(firstName);
+
+            Console.WriteLine(lastName);
+
+            Console.WriteLine(hobby);
+
+            Console.WriteLine(age);
+
+            Console.WriteLine(year);
+
+            Console.WriteLine(favoriteNumber);
+
+            Console.WriteLine(temperature);
+
+            Console.WriteLine(pi);
+
+            Console.WriteLine(height);
+
+            Console.WriteLine(isStudent);
+
+            Console.WriteLine(likesPizza);
+
+            Console.WriteLine(hasPet);
+
+            Console.WriteLine(grade);
+
+            Console.WriteLine(middleInitial);
+
+            Console.WriteLine(middleInitial2);
+
+            Console.WriteLine(currencySymbol);
+
+
+
+        }
+    }
+}
